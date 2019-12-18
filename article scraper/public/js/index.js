3813d35ddfab26737c593da19045bb1a55e20a53
Dibora D <diborad@yahoo.com>
2019-12-18 15:06:01 -0600

    set up API routes, CREATE new article and DELETE article and export modules

--- article scraper/public/js/index.js
+++ article scraper/public/js/index.js
@@ -1,47 +1,49 @@
 // Get references to page elements
 var $exampleText = $("#example-text");
 var $exampleDescription = $("#example-description");
+var $articleText = $("#article-text");
+var $articleDescription = $("#article-description");
 var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+var $articleList = $("#article-list");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveArticle: function(article) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
       },
       type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
+      url: "api/articles",
+      data: JSON.stringify(article)
     });
   },
-  getExamples: function() {
+  getArticles: function() {
     return $.ajax({
-      url: "api/examples",
+      url: "api/articles",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  deleteArticle: function(id) {
     return $.ajax({
-      url: "api/examples/" + id,
+      url: "api/articles/" + id,
       type: "DELETE"
     });
   }
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
+var refreshArticles = function() {
+  API.getArticles ().then(function(data) {
+    var $articles  = data.map(function(article) {
       var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
+        .text(articles .text)
+        .attr("href", "/article/" + article.id);
 
       var $li = $("<li>")
         .attr({
           class: "list-group-item",
-          "data-id": example.id
+          "data-id": article.id
         })
         .append($a);
 
@@ -54,8 +56,8 @@ var refreshExamples = function() {
       return $li;
     });
 
-    $exampleList.empty();
-    $exampleList.append($examples);
+    $articleList.empty();
+    $articleList.append($articles);
   });
 };
 
@@ -64,22 +66,23 @@ var refreshExamples = function() {
 var handleFormSubmit = function(event) {
   event.preventDefault();
 
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
+  var article = {
+    text: $articleText.val().trim(),
+    description: $articleDescription.val().trim()
   };
 
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
+  if (!(article.text && article.description)) {
+    alert("You must enter an text and description!");
     return;
   }
 
-  API.saveExample(example).then(function() {
-    refreshExamples();
+  API.saveArticle(article)
+  .then(function() {
+  refreshArticles();
   });
 
-  $exampleText.val("");
-  $exampleDescription.val("");
+  $articleText.val("");
+  $articleDescription.val("");
 };
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
@@ -89,11 +92,11 @@ var handleDeleteBtnClick = function() {
     .parent()
     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
+  API.deleteArticle(idToDelete).then(function() {
+    refreshArticle();
   });
 };
 
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+$articleList.on("click", ".delete", handleDeleteBtnClick);
