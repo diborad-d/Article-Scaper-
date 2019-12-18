509136fb2fa9bce500638d8baea4dda4cf3e6146
Dibora D <diborad@yahoo.com>
2019-12-18 14:59:41 -0600

    set up the MODEL part of the app: mongodb(mongoose) for the scraped articles and comments page

---
+++ article scraper/models/Comment.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+var CommentSchema = new Schema({
+  name: {
+    type:String,
+   
+  },
+  body: {
+    type:String,
+    require: true
+  },
+  
+})
+var Comment = mongoose.model("Article", CommnetSchema);
+module.exports = Comment;
